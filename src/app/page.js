5a52e4199cca3c129bb06eb010ec83530e25bd65
sahilsharma <sahilsharma75831@gmail.com>
2024-03-12 19:04:08 +0530

    second

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function page() {
-  return <div>page</div>;
+  return (
+    <div className="flex justify-center items-center w-full h-full fixed">
+      <img src="https://media0.giphy.com/media/0Yc8mk9PwoqvepYRQX/giphy.gif?cid=6c09b9520gnt0d26lbb6f3f0of7ychjl8q1gqrd3hlzy3ecd&ep=v1_stickers_related&rid=giphy.gif&ct=s" />
+    </div>
+  );
 }
 
 export default page;
